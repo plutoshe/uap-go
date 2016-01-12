65da3eac6e7942ebcb7a4108a72ad27f4485cbad
plutoshe <plutoshe@gmail.com>
2016-01-12 10:49:30 +0800

    channel: add leastversion condition

--- uaparser/channel.go
+++ uaparser/channel.go
@@ -2,6 +2,7 @@ package uaparser
 
 import (
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -10,12 +11,38 @@ type Channel struct {
 }
 
 type ChannelPattern struct {
-	Model           string
-	Regexp          *regexp.Regexp
-	Regex           string
-	ExactMatch      string
-	UserAgentFamily string
-	MajorVersion    string
+	Model             string
+	Regexp            *regexp.Regexp
+	Regex             string
+	ExactMatch        string
+	UserAgentFamily   string
+	LeastMajorVersion string
+}
+
+func (channelPattern *ChannelPattern) leastMajorVersionJudgement(cli *Client, line string) bool {
+	if channelPattern.LeastMajorVersion == "" {
+		return true
+	}
+
+	cm, err := strconv.Atoi(channelPattern.LeastMajorVersion)
+	if err != nil {
+		return false
+	}
+	um, err := strconv.Atoi(cli.UserAgent.Major)
+	if err != nil {
+		return false
+	}
+	if um >= cm {
+		return true
+	}
+	return false
+}
+
+func (channelPattern *ChannelPattern) userAgentJudgement(cli *Client, line string) bool {
+	if channelPattern.UserAgentFamily != "" && strings.Contains(cli.UserAgent.Family, channelPattern.UserAgentFamily) {
+		return true
+	}
+	return false
 }
 
 func (channelPattern *ChannelPattern) Match(cli *Client, line string) bool {
@@ -28,7 +55,7 @@ func (channelPattern *ChannelPattern) Match(cli *Client, line string) bool {
 			return true
 		}
 	}
-	if channelPattern.UserAgentFamily != "" && strings.Contains(cli.UserAgent.Family, channelPattern.UserAgentFamily) {
+	if channelPattern.userAgentJudgement(cli, line) && channelPattern.leastMajorVersionJudgement(cli, line) {
 		return true
 	}
 	return false
